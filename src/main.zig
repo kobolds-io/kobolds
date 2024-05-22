bc2a8a98f5a6ce36d8072f5b6f141afc892cb1c6
Ben Hodge <benjamin.andrew.hodge@gmail.com>
2024-05-22 11:42:56 -0500

    got a working bigendian reader

--- src/main.zig
+++ src/main.zig
@@ -1,26 +1,21 @@
 const std = @import("std");
+const parser = @import("./parser.zig");
 
 pub fn main() !void {
-    // const data: [4]u8 = [4]u8{0x12, 0x34, 0x56, 0x78};
-    const data: []const u8 = &[_]u8{ 0, 0, 0, 5, 104, 101, 108, 108, 111 };
-    const result = bytesToU32BigEndian(data[0..4]) catch {
-        std.debug.print("Error: Invalid byte array length\n", .{});
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const allocator = gpa.allocator();
+    errdefer _ = gpa.deinit();
+
+    // 05hello
+    const raw_bytes = [_]u8{ 0, 0, 0, 5, 104, 101, 108, 108, 111 };
+    const bytes_mem = std.mem.sliceAsBytes(raw_bytes[0..4]);
+    var stream = std.io.fixedBufferStream(bytes_mem);
+
+    // this is the length of the
+    const result = parser.readU32sFromReader(allocator, stream.reader()) catch |err| {
+        std.debug.print("Error: Invalid byte array length {any}\n", .{err});
         return;
     };
 
-    std.debug.print("The u32 value is: {x}\n", .{result});
-}
-
-// Todo convert 4 bytes to u32 from bigendian
-fn bytesToU32BigEndian(bytes: []const u8) !u32 {
-    if (bytes.len != 4) {
-        return error.InvalidByteArrayLength;
-    }
-
-    const a: u8 = @intCast(bytes[0] << 24);
-    const b: u8 = @intCast(bytes[1] << 16);
-    const c: u8 = @intCast(bytes[2] << 8);
-    const d: u8 = @intCast(bytes[3]);
-
-    return a | b | c | d;
+    std.debug.print("The u32 value is: {any}\n", .{result.items});
 }
--- src/parser.zig
+++ src/parser.zig
@@ -62,3 +62,56 @@ const std = @import("std");
 //     std.debug.print("Message: {s}\n", .{message});
 // }
 // }
+
+pub const ParseError = error{ReceivedInvalidBytes};
+
+pub fn readU32sFromReader(allocator: std.mem.Allocator, reader: anytype) !std.ArrayList(u32) {
+    var buffered_reader = std.io.bufferedReader(reader);
+    var numbers = std.ArrayList(u32).init(allocator);
+    var parsed_number_buffer: [4]u8 = undefined;
+    while (true) {
+        const bytes_read = try buffered_reader.read(&parsed_number_buffer);
+        if (bytes_read == 0) {
+            break;
+        }
+        if (bytes_read != 4) {
+            // Decide what to do if there's not exactly 4 bytes.
+            // For now, we'll just error.
+            return ParseError.ReceivedInvalidBytes;
+        }
+        const parsed_num = std.mem.readInt(u32, &parsed_number_buffer, .big);
+        try numbers.append(parsed_num);
+    }
+
+    return numbers;
+}
+
+test "convert big endian bytes to an ArrayList of u32s" {
+    // setup
+    const bytes = [_]u8{ 0, 0, 0, 5 };
+    const bytes_mem = std.mem.sliceAsBytes(&bytes);
+    var stream = std.io.fixedBufferStream(bytes_mem);
+
+    const want: u32 = 5;
+    const got = try readU32sFromReader(std.testing.allocator, stream.reader());
+    defer got.deinit();
+
+    try std.testing.expectEqual(got.items.len, 1);
+    try std.testing.expectEqual(want, got.items[0]);
+}
+
+test "convert big endian bytes returns error if invalid bytes" {
+    // setup
+    // the size of the byte array is larger than what the algorithm handles
+    const bytes = [_]u8{ 0, 0, 0, 5, 0, 0 };
+    const bytes_mem = std.mem.sliceAsBytes(&bytes);
+    var stream = std.io.fixedBufferStream(bytes_mem);
+
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const allocator = gpa.allocator();
+    // only deinit if something goes wrong with the test
+    errdefer _ = gpa.deinit();
+
+    const got = readU32sFromReader(allocator, stream.reader());
+    try std.testing.expectEqual(ParseError.ReceivedInvalidBytes, got);
+}
