ffa937e1bc02fc92e343be6239e2e4fa08a8314b
Ben Hodge <benjamin.andrew.hodge@gmail.com>
2024-05-23 22:38:21 -0500

    remove comment

--- src/main.zig
+++ src/main.zig
@@ -24,19 +24,4 @@ pub fn main() !void {
     std.debug.print("took {}ns\n", .{took});
     std.debug.print("took {}us\n", .{took / std.time.ns_per_us});
     std.debug.print("took {}ms\n", .{took / std.time.ns_per_ms});
-
-    // for (msgs) |msg| {
-    // std.debug.print("Message! {any}\n", .{msg});
-    // }
-
-    // const bytes_mem = std.mem.sliceAsBytes(raw_bytes[0..4]);
-    // var stream = std.io.fixedBufferStream(bytes_mem);
-    //
-    // // this is the length of the
-    // const num = parser.beToU32(allocator, stream.reader()) catch |err| {
-    //     std.debug.print("Error: Invalid byte array length {any}\n", .{err});
-    //     return;
-    // };
-
-    // std.debug.print("The u32 value is: {any}\n", .{num});
 }
