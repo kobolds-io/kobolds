bfdd8def46f0669c764e5b178cba6cf71a02fbc4
Ben Hodge <benjamin.andrew.hodge@gmail.com>
2024-07-13 10:07:53 -0500

    add converter from u32 to u8

--- src/main.zig
+++ src/main.zig
@@ -2,87 +2,26 @@ const std = @import("std");
 const net = std.net;
 const Node = @import("./proto/node.zig").Node;
 const MessageParser = @import("./proto/parser.zig").MessageParser;
+const Message = @import("./proto/message.zig").Message;
+const MessageType = @import("./proto/message.zig").MessageType;
 
 pub fn main() !void {
-    // var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-    // const allocator = gpa.allocator();
-    // defer _ = gpa.deinit();
-    //
-    // const address = try net.Address.parseIp("127.0.0.1", 4000);
-    // var listener = try address.listen(.{
-    //     .reuse_address = true,
-    //     .kernel_backlog = 1024,
-    // });
-    // defer listener.deinit();
-    // std.log.info("listening at {any}\n", .{address});
-    //
-    // var nh = Node.new(allocator);
-    //
-    // // thread pool time!
-    // var pool: std.Thread.Pool = undefined;
-    // // I added 32 jobs to spawn 32 worker threads but idk, likely can do more
-    // try pool.init(.{ .allocator = allocator, .n_jobs = 32 });
-    // defer pool.deinit();
-    //
-    // while (true) {
-    //     var conn = listener.accept() catch |err| {
-    //         std.debug.print("could not accept connection {}", .{err});
-    //         continue;
-    //     };
-    //
-    //     // _ = try node.add_connection(conn);
-    //
-    //     try pool.spawn(Node.handle_connection, .{ &nh, &conn });
-    // }
+    // create a message
+    const original_msg = Message.new("1", "/hello", "world");
 
-    // TODO: parse a config file to populate env variables
+    // serialize it
+    var bytes_gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const bytes_allocator = bytes_gpa.allocator();
+    defer _ = bytes_gpa.deinit();
 
-    var buffer_gpa = std.heap.GeneralPurposeAllocator(.{}){};
-    const buffer_allocator = buffer_gpa.allocator();
-    defer _ = buffer_gpa.deinit();
-    var parser = MessageParser.init(buffer_allocator);
+    var bytes = std.ArrayList(u8).init(bytes_allocator);
+    defer bytes.deinit();
 
-    var parsed_messages_gpa = std.heap.GeneralPurposeAllocator(.{}){};
-    const parsed_messages_allocator = parsed_messages_gpa.allocator();
-    defer _ = parsed_messages_gpa.deinit();
+    try original_msg.cborStringify(.{}, bytes.writer());
 
-    var parsed_messages = std.ArrayList([]u8).init(parsed_messages_allocator);
-    defer _ = parsed_messages.deinit();
+    // generate a length prefixed payload
 
-    // 05hello
-    const data = [_]u8{ 0, 0, 0, 5, 104, 101, 108, 108, 111 };
-    try parser.parse(&parsed_messages, &data);
-
-    std.debug.print("parsed_messages.items {any}\n", .{parsed_messages.items});
-
-    // var messages = std.ArrayList([]const u8).init(messages_allocator);
-    // defer messages.deinit();
-    //
-    // // var parser = MessageParser.new();
-    //
-    // // 05hello07hellooo
-    // // const bytes = [_]u8{ 0, 0, 0, 5, 104, 101, 108, 108, 111, 0, 0, 0, 7, 104, 101, 108, 108, 111, 111, 111 };
-    // // 05hello
-    // const bytes = [_]u8{ 0, 0, 0, 5, 104, 101, 108, 108, 111 };
-    // var timer = try std.time.Timer.start();
-    //
-    // const iters = 1;
-    // std.debug.print("sending {} bytes through parser {} times\n", .{ bytes.len, iters });
-    //
-    // var buffer: [1024]u8 = undefined;
-    // var end_idx: usize = 0;
-    //
-    // for (0..iters) |_| {
-    //     // std.debug.print("i {d}\n", .{i});
-    //     // std.time.sleep(std.time.ns_per_ms * 1);
-    //     // _ = try parser.parse(&messages, &bytes);
-    //     _ = try MessageParser.parse(&messages, &bytes, &buffer, &end_idx);
-    // }
-
-    // std.debug.print("messages {any}\n", .{messages.items});
-    //
-    // const took = timer.read();
-    // std.debug.print("took {}ns\n", .{took});
-    // std.debug.print("took {}us\n", .{took / std.time.ns_per_us});
-    // std.debug.print("took {}ms\n", .{took / std.time.ns_per_ms});
+    // parse the length prefixed payload
+    // deserialize the message
+    // verify that the message is the same
 }
--- src/proto/utils.zig
+++ src/proto/utils.zig
@@ -1,3 +1,5 @@
+const std = @import("std");
+
 /// Compares two slices and returns whether they are equal.
 pub fn eql(comptime T: type, a: []const T, b: []const T) bool {
     if (a.len != b.len) return false;
@@ -6,4 +8,48 @@ pub fn eql(comptime T: type, a: []const T, b: []const T) bool {
         if (a_elem != b_elem) return false;
     }
     return true;
+}
+
+pub fn beToU32(bytes: *[4]u8) u32 {
+    return std.mem.readInt(u32, bytes, .big);
+}
+
+fn u32ToBytes(value: u32) [4]u8 {
+    return [_]u8{
+        @intCast((value >> 24) & 0xFF),
+        @intCast((value >> 16) & 0xFF),
+        @intCast((value >> 8) & 0xFF),
+        @intCast(value & 0xFF),
+    };
+}
+
+test beToU32 {
+    var buf = [_]u8{ 0, 0, 0, 5, 1, 1, 1, 1, 1 };
+    const bytes = buf[0..4];
+    const want: u32 = 5;
+
+    const got = beToU32(bytes);
+
+    try std.testing.expectEqual(want, got);
+}
+
+test u32ToBytes {
+    const value1: u32 = 5;
+    const bytes1 = u32ToBytes(value1);
+    const want1 = [4]u8{ 0, 0, 0, 5 };
+
+    try std.testing.expect(eql(u8, &want1, &bytes1));
+
+    const value2: u32 = 256;
+    const bytes2 = u32ToBytes(value2);
+    const want2 = [4]u8{ 0, 0, 1, 0 };
+
+    try std.testing.expect(eql(u8, &want2, &bytes2));
+
+    const value3: u32 = 3000;
+    const bytes3 = u32ToBytes(value3);
+    std.debug.print("bytes {any}\n", .{bytes3});
+    const want3 = [4]u8{ 0, 0, 11, 184 };
+
+    try std.testing.expect(eql(u8, &want3, &bytes3));
 }
