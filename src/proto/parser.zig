8f3c4f8bca595a0e287a01019606305826778dae
Ben Hodge <benjamin.andrew.hodge@gmail.com>
2024-07-15 12:16:43 -0500

    Refactor parser (#13)
    Refactor the parser to use std.mem.copyForwards and adds a verbose test runner
    
    * renaming
    
    * working on figuring out the deinit issue
    
    * i'm stumped
    
    * fixing up the parser
    
    * cleanup
    
    * parser has been refactored to use copyForwards
    
    * remove example from main
    
    * add a test runner with more verbose output

--- src/proto/parser.zig
+++ src/proto/parser.zig
@@ -3,22 +3,21 @@ const utils = @import("./utils.zig");
 
 const ParseError = error{
     InvalidInputData,
-    NotEnoughCapacity,
 };
 
-pub const MessageParser = struct {
+pub const Parser = struct {
     const Self = @This();
 
-    buffer: std.ArrayList(u8) = undefined,
+    buffer: std.ArrayList(u8),
 
-    pub fn init(allocator: std.mem.Allocator) MessageParser {
-        return MessageParser{
+    pub fn init(allocator: std.mem.Allocator) Parser {
+        return Parser{
             .buffer = std.ArrayList(u8).init(allocator),
         };
     }
 
-    // idk if i should deinit this or not. i think so
     pub fn deinit(self: *Self) void {
+        // this function call fails. I suspect a double free somewhere but I cannot find it
         self.buffer.deinit();
     }
 
@@ -41,7 +40,17 @@ pub const MessageParser = struct {
                 try parsed_messages.append(message);
 
                 // reset the buffer
-                self.buffer.items = self.buffer.items[4 + message_length ..];
+                // clear out the mem this doesn't really work
+                // self.buffer.replaceRangeAssumeCapacity(
+                //     0,
+                //     4 + message_length,
+                //     &.{},
+                // );
+
+                // this is directly manipulating the underlying memory
+                // feels kind of gross but in the end it works.
+                std.mem.copyForwards(u8, self.buffer.items, self.buffer.items[4 + message_length ..]);
+                self.buffer.items.len -= (4 + message_length);
             } else {
                 // Incomplete message in the buffer, wait for more data
                 break;
@@ -49,13 +58,51 @@ pub const MessageParser = struct {
         }
     }
 };
+test "it deinits correctly" {
+    var input_gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const input_allocator = input_gpa.allocator();
+    defer _ = input_gpa.deinit();
+
+    var input_data = std.ArrayList(u8).init(input_allocator);
+    defer input_data.deinit();
+
+    const MESSAGES_COUNT: u32 = 10_000;
+    const input = [_]u8{ 0, 0, 0, 6, 104, 101, 108, 108, 111, 111 };
+
+    // make a very large input
+    for (0..MESSAGES_COUNT) |_| {
+        try input_data.appendSlice(&input);
+    }
+
+    var messages_gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const messages_allocator = messages_gpa.allocator();
+    defer _ = messages_gpa.deinit();
+
+    var parsed_messages = std.ArrayList([]u8).init(messages_allocator);
+    defer parsed_messages.deinit();
+
+    var parser_gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const parser_allocator = parser_gpa.allocator();
+    defer _ = parser_gpa.deinit();
+
+    var parser = Parser.init(parser_allocator);
+    defer parser.deinit();
+
+    // Run the parser
+    try parser.parse(&parsed_messages, input_data.items);
+    // Success for this test means that it exits correctly and doesn't segfault
+}
 
 test "error if input data is invalid" {
     var messages = std.ArrayList([]u8).init(std.testing.allocator);
     const input_data = [_]u8{ 0, 0, 0 };
     try std.testing.expect(input_data.len < 4);
 
-    var parser = MessageParser.init(std.testing.allocator);
+    var parser_arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    const parser_allocator = parser_arena.allocator();
+    defer parser_arena.deinit();
+
+    var parser = Parser.init(parser_allocator);
     defer parser.deinit();
 
     const result = parser.parse(&messages, &input_data);
@@ -63,16 +110,25 @@ test "error if input data is invalid" {
 }
 
 test "parses length prefixed messages" {
+    // Allocator that lives outside of the parser
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     const allocator = gpa.allocator();
+    defer _ = gpa.deinit();
+
     var parsed_messages = std.ArrayList([]u8).init(allocator);
+    defer parsed_messages.deinit();
+
     try std.testing.expectEqual(0, parsed_messages.items.len);
 
     const input_data = [_]u8{ 0, 0, 0, 5, 104, 101, 108, 108, 111, 0, 0, 0, 7, 104, 101, 108, 108, 111, 111, 111 };
     const want_1 = [_]u8{ 104, 101, 108, 108, 111 };
     const want_2 = [_]u8{ 104, 101, 108, 108, 111, 111, 111 };
 
-    var parser = MessageParser.init(std.testing.allocator);
+    var parser_arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    const parser_allocator = parser_arena.allocator();
+    defer parser_arena.deinit();
+
+    var parser = Parser.init(parser_allocator);
     defer parser.deinit();
 
     try parser.parse(&parsed_messages, &input_data);
@@ -83,29 +139,46 @@ test "parses length prefixed messages" {
 }
 
 test "parses 100_000 10 byte messages in less than 5 milliseconds using a single call" {
-    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-    const allocator = gpa.allocator();
-    var parsed_messages = std.ArrayList([]u8).init(allocator);
+    var input_gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const input_allocator = input_gpa.allocator();
+    defer _ = input_gpa.deinit();
+
+    var input_data = std.ArrayList(u8).init(input_allocator);
+    defer input_data.deinit();
+
+    const MESSAGES_COUNT: u32 = 1000;
+    const input = [_]u8{ 0, 0, 0, 6, 104, 101, 108, 108, 111, 111 };
+
+    for (0..MESSAGES_COUNT) |_| {
+        try input_data.appendSlice(&input);
+    }
+
+    try std.testing.expectEqual(MESSAGES_COUNT * input.len, input_data.items.len);
+
+    var messages_gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const messages_allocator = messages_gpa.allocator();
+    defer _ = messages_gpa.deinit();
+
+    var parsed_messages = std.ArrayList([]u8).init(messages_allocator);
+    defer parsed_messages.deinit();
 
     try std.testing.expectEqual(0, parsed_messages.items.len);
 
-    const MESSAGES_COUNT: u32 = 100_000;
-    const input_data = comptime [_]u8{ 0, 0, 0, 6, 104, 101, 108, 108, 111, 111 } ** MESSAGES_COUNT;
+    var parser_gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const parser_allocator = parser_gpa.allocator();
+    defer _ = parser_gpa.deinit();
 
-    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
-    const buffer_allocator = arena.allocator();
-
-    // Initialize the parser here
-    var parser = MessageParser.init(buffer_allocator);
+    var parser = Parser.init(parser_allocator);
+    defer parser.deinit();
 
     var timer = try std.time.Timer.start();
     defer timer.reset();
 
-    try parser.parse(&parsed_messages, &input_data);
+    try parser.parse(&parsed_messages, input_data.items);
     const duration = timer.read();
 
+    // std.debug.print("duration {any}us\n", .{duration / std.time.ns_per_us});
     try std.testing.expect((duration / std.time.ns_per_ms) <= 5);
-
     try std.testing.expectEqual(MESSAGES_COUNT, parsed_messages.items.len);
 
     const want = [_]u8{ 104, 101, 108, 108, 111, 111 };
@@ -119,20 +192,22 @@ test "parses incomplete messages with multiple parse calls" {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     const allocator = gpa.allocator();
     var parsed_messages = std.ArrayList([]u8).init(allocator);
+
     try std.testing.expectEqual(0, parsed_messages.items.len);
 
     const input_data_1 = [_]u8{ 0, 0, 0, 5, 104, 101, 108 };
     const input_data_2 = [_]u8{ 108, 111 };
     const want = [_]u8{ 104, 101, 108, 108, 111 };
 
-    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
-    defer arena.deinit();
-    const buffer_allocator = arena.allocator();
+    var parser_gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const parser_allocator = parser_gpa.allocator();
+    defer _ = parser_gpa.deinit();
 
-    var parser = MessageParser.init(buffer_allocator);
-    // defer parser.deinit();
+    var parser = Parser.init(parser_allocator);
+    defer parser.deinit();
 
     try parser.parse(&parsed_messages, &input_data_1);
+
     try std.testing.expectEqual(0, parsed_messages.items.len);
 
     try parser.parse(&parsed_messages, &input_data_2);
