daab394b4d42ebefa5ac55b312d22a99e37cdb8e
Ben Hodge <benjamin.andrew.hodge@gmail.com>
2025-08-06 23:31:34 -0500

    add authenticator interface
    This feels way more complicated than it should be. I kind of like the
    idea of using unions instead.

--- src/node/authenticator.zig
+++ src/node/authenticator.zig
@@ -1,67 +1,165 @@
 const std = @import("std");
 const testing = std.testing;
 
-pub const AuthenticatorConfig = struct {
-    authentication_strategy: AuthenticationStrategy = .{ .none = {} },
-};
-
-pub const AuthenticationStrategy = union(AuthenticationType) {
-    auth_token: AuthTokenStrategy,
-    none: void,
-};
-
-pub const AuthTokenStrategy = struct {
-    /// if there are no tokens defined then no one is authorized
-    allowed_token: []const u8 = undefined,
-};
-
-const AuthenticationType = enum {
-    auth_token,
-    none,
-};
-
 pub const Authenticator = struct {
     const Self = @This();
 
-    allocator: std.mem.Allocator,
-    config: AuthenticatorConfig,
+    pub const Context = struct {
+        origin_id: u128,
+        connection_id: u128,
+    };
 
-    pub fn init(allocator: std.mem.Allocator, config: AuthenticatorConfig) Self {
+    /// The interface to be implemented by strategies
+    pub const StrategyInterface = struct {
+        authenticate_fn: *const fn (self: *anyopaque, context: Context) bool,
+        deinit_fn: *const fn (self: *anyopaque) void,
+    };
+
+    allocator: std.mem.Allocator,
+    strategy: *anyopaque,
+    vtable: *const StrategyInterface,
+
+    pub fn init(
+        allocator: std.mem.Allocator,
+        comptime T: type,
+        strategy: *T,
+        vtable: *const StrategyInterface,
+    ) Self {
         return Self{
             .allocator = allocator,
-            .config = config,
+            .strategy = strategy,
+            .vtable = vtable,
         };
     }
 
-    pub fn deinit(self: *Self) void {
-        _ = self;
+    pub fn authenticate(self: *Self, context: Context) bool {
+        return self.vtable.authenticate_fn(self.strategy, context);
     }
 
-    pub fn authenticate(self: *Self, context: anytype) bool {
-        _ = context;
-        switch (self.config.authentication_strategy) {
-            .none => return true,
-            .auth_token => return false,
-        }
+    pub fn deinit(self: *Self) void {
+        self.vtable.deinit_fn(self.strategy);
+    }
+};
+
+pub const NoneAuthStrategy = struct {
+    const Self = @This();
+    pub const VTable = Authenticator.StrategyInterface{
+        .authenticate_fn = &Self.authenticate,
+        .deinit_fn = &Self.deinit,
+    };
+
+    pub fn authenticate(_: *anyopaque, _: Authenticator.Context) bool {
+        return true;
+    }
+
+    pub fn deinit(_: *anyopaque) void {}
+};
+
+pub const TokenAuthStrategy = struct {
+    const Self = @This();
+
+    pub const VTable = Authenticator.StrategyInterface{
+        .authenticate_fn = &Self.authenticate,
+        .deinit_fn = &Self.deinit,
+    };
+
+    const TokenEntry = struct {
+        token: []const u8,
+    };
+
+    allocator: std.mem.Allocator,
+    allowed_tokens: std.ArrayList(TokenEntry),
+
+    pub fn init(allocator: std.mem.Allocator) Self {
+        return Self{
+            .allocator = allocator,
+            .allowed_tokens = std.ArrayList(TokenEntry).init(allocator),
+        };
+    }
+
+    pub fn authenticate(ptr: *anyopaque, _: Authenticator.Context) bool {
+        const self: *Self = @ptrCast(@alignCast(ptr));
+        std.debug.print("self {any}\n", .{self});
+        return true;
+    }
+
+    pub fn deinit(ptr: *anyopaque) void {
+        const self: *Self = @ptrCast(@alignCast(ptr));
+
+        self.allowed_tokens.deinit();
     }
 };
 
 test "init/deinit" {
-    const allocator = testing.allocator;
+    // const allocator = testing.allocator;
 
-    var authenticator = Authenticator.init(allocator, .{});
-    defer authenticator.deinit();
+    // var authenticator = Authenticator.init(allocator, .{});
+    // defer authenticator.deinit();
 }
 
 test "none strategy" {
     const allocator = testing.allocator;
 
-    const config = AuthenticatorConfig{
-        .authentication_strategy = AuthenticationStrategy{ .none = {} },
-    };
-
-    var authenticator = Authenticator.init(allocator, config);
+    var none_auth_strategy = NoneAuthStrategy{};
+    var authenticator = Authenticator.init(
+        allocator,
+        NoneAuthStrategy,
+        &none_auth_strategy,
+        &NoneAuthStrategy.VTable,
+    );
     defer authenticator.deinit();
 
-    try testing.expectEqual(true, authenticator.authenticate({}));
+    const context = Authenticator.Context{
+        .origin_id = 123,
+        .connection_id = 432,
+    };
+
+    try testing.expectEqual(true, authenticator.authenticate(context));
 }
+
+test "token strategy" {
+    const allocator = testing.allocator;
+
+    var token_auth_strategy = TokenAuthStrategy.init(allocator);
+    try token_auth_strategy.allowed_tokens.append(.{ .token = "asdf" });
+    try token_auth_strategy.allowed_tokens.append(.{ .token = "asdfasdf" });
+
+    var authenticator = Authenticator.init(
+        allocator,
+        TokenAuthStrategy,
+        &token_auth_strategy,
+        &TokenAuthStrategy.VTable,
+    );
+    defer authenticator.deinit();
+
+    const context = Authenticator.Context{
+        .origin_id = 123,
+        .connection_id = 432,
+    };
+
+    try testing.expectEqual(true, authenticator.authenticate(context));
+}
+
+// test "auth_token strategy" {
+//     const allocator = testing.allocator;
+
+//     const allowed_tokens: [][]const u8 = .{"asdf"};
+//     const config = AuthenticatorConfig{
+//         .authentication_strategy = AuthenticationStrategy{
+//             .auth_token = AuthTokenStrategy{
+//                 .allowed_tokens = &allowed_tokens,
+//             },
+//         },
+//     };
+
+//     var authenticator = Authenticator.init(allocator, config);
+//     defer authenticator.deinit();
+
+//     const context = Authenticator.Context{
+//         .origin_id = 123,
+//         .connection_id = 432,
+//     };
+
+//     try testing.expectEqual(.auth_token, authenticator.config.authentication_strategy);
+//     try testing.expectEqual(true, authenticator.authenticate(context));
+// }
