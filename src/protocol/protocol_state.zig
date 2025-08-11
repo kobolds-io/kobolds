1c411962334e7291addba53de3212b9da297353a
Ben Hodge <benjamin.andrew.hodge@gmail.com>
2025-08-11 15:49:03 +0000

    making protocol state more verbose

--- src/protocol/protocol_state.zig
+++ src/protocol/protocol_state.zig
@@ -1,8 +1,33 @@
 pub const ProtocolState = enum {
-    inactive,
-    awaiting_accept,
-    authenticating,
-    ready,
-    terminating,
-    terminated,
+    inactive, // Client/Node
+    sending_accept, // Node
+    awaiting_accept, // Client
+    sending_challenge, // Node
+    awaiting_challenge, // Client
+    awaiting_credentials, // Node
+    sending_credentials, // Client
+    awaiting_authentication, // Client
+    ready, // Client/Node
+    terminating, // Client/Node
+    terminated, // Client/Node
 };
+
+// node perpective
+// 1. inactive (inital state) -> sending accept
+// 2. sending accept -> sending auth challenge
+// 3. sending auth challenge -> awaiting_auth_credentials/terminating
+// 4. awaiting auth credentials -> authenticating/terminating
+// 5. authenticating -> ready/terminating
+// 6. ready
+// 7. terminating
+// 8. terminated
+//
+// client perpective
+// 1. inactive (inital state)
+// 2. awaiting accept
+// 3. awaiting auth challenge
+// 4. sending auth credentials
+// 5. awaiting auth reply
+// 6. ready
+// 7. terminating
+// 8. terminated
