e72c07c77dadddb308598162f075db6296992d6d
Ben Hodge <benjamin.andrew.hodge@gmail.com>
2025-09-13 00:22:27 -0500

    add parser2 benchmark
    It is significantly faster than my original parser

--- src/bench.zig
+++ src/bench.zig
@@ -14,5 +14,6 @@ comptime {
     _ = @import("./benchmarks/checksum.zig");
     _ = @import("./benchmarks/message.zig");
     _ = @import("./benchmarks/message2.zig");
-    // _ = @import("./benchmarks/parser.zig");
+    _ = @import("./benchmarks/parser.zig");
+    _ = @import("./benchmarks/parser2.zig");
 }
--- src/benchmarks/parser2.zig
+++ src/benchmarks/parser2.zig
@@ -3,18 +3,18 @@ const zbench = @import("zbench");
 const assert = std.debug.assert;
 const testing = std.testing;
 
-const Parser = @import("../protocol/parser.zig").Parser;
-const Message = @import("../protocol/message.zig").Message;
+const Parser = @import("../protocol/parser2.zig").Parser;
+const Message = @import("../protocol/message2.zig").Message;
 
 const constants = @import("../constants.zig");
 const benchmark_constants = @import("./constants.zig");
 
 const ParserParseBenchmark = struct {
-    messages: *std.array_list.Managed(Message),
+    messages: []Message,
     parser: *Parser,
     bytes: []const u8,
 
-    fn new(messages: *std.array_list.Managed(Message), parser: *Parser, bytes: []const u8) ParserParseBenchmark {
+    fn new(messages: []Message, parser: *Parser, bytes: []const u8) ParserParseBenchmark {
         return .{
             .messages = messages,
             .parser = parser,
@@ -23,7 +23,7 @@ const ParserParseBenchmark = struct {
     }
 
     pub fn run(self: ParserParseBenchmark, _: std.mem.Allocator) void {
-        self.parser.parse(self.messages, self.bytes) catch unreachable;
+        _ = self.parser.parse(self.messages, self.bytes) catch unreachable;
     }
 };
 
@@ -33,7 +33,7 @@ fn afterEach() void {
     parser_messages.items.len = 0;
 }
 
-var parser_messages: std.array_list.Managed(Message) = undefined;
+var parser_messages: std.ArrayList(Message) = undefined;
 
 test "Parser benchmarks" {
     var bench = zbench.Benchmark.init(std.testing.allocator, .{
@@ -45,22 +45,41 @@ test "Parser benchmarks" {
     defer _ = parser_messages_gpa.deinit();
     const parser_messages_allocator = parser_messages_gpa.allocator();
 
-    parser_messages = std.array_list.Managed(Message).initCapacity(parser_messages_allocator, std.math.maxInt(u16)) catch unreachable;
-    defer parser_messages.deinit();
+    parser_messages = std.ArrayList(Message).initCapacity(parser_messages_allocator, std.math.maxInt(u16)) catch unreachable;
+    defer parser_messages.deinit(parser_messages_allocator);
 
     var parser_gpa = std.heap.GeneralPurposeAllocator(.{}).init;
     defer _ = parser_gpa.deinit();
     const parser_allocator = parser_gpa.allocator();
 
-    var parser = Parser.init(parser_allocator);
-    defer parser.deinit();
-
-    const headers_1 = [_]u8{ 185, 109, 74, 197, 189, 38, 87, 150, 98, 99, 131, 40, 248, 184, 217, 148, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 32, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 47, 104, 101, 108, 108, 111, 47, 119, 111, 114, 108, 100 };
+    var parser = try Parser.init(parser_allocator);
+    defer parser.deinit(parser_allocator);
 
     const body = [_]u8{97} ** constants.message_max_body_size;
-    const bytes_1 = headers_1 ++ body;
-    const bytes_2 = bytes_1 ++ headers_1 ++ body;
-    const bytes_3 = bytes_2 ++ headers_1 ++ body;
+    var message_1 = Message.new(.undefined);
+    message_1.setBody(&body);
+
+    const message_1_buf = try parser_messages_allocator.alloc(u8, message_1.packedSize());
+    defer parser_messages_allocator.free(message_1_buf);
+
+    const serialized_bytes_1_count = message_1.serialize(message_1_buf);
+
+    const bytes_1 = try parser_messages_allocator.alloc(u8, serialized_bytes_1_count * 1);
+    defer parser_messages_allocator.free(bytes_1);
+    const bytes_2 = try parser_messages_allocator.alloc(u8, serialized_bytes_1_count * 2);
+    defer parser_messages_allocator.free(bytes_2);
+    const bytes_3 = try parser_messages_allocator.alloc(u8, serialized_bytes_1_count * 3);
+    defer parser_messages_allocator.free(bytes_3);
+
+    const serialized_message = message_1_buf[0..serialized_bytes_1_count];
+    @memcpy(bytes_1, serialized_message);
+
+    @memcpy(bytes_2[0..serialized_bytes_1_count], serialized_message);
+    @memcpy(bytes_2[serialized_bytes_1_count .. serialized_bytes_1_count * 2], serialized_message);
+
+    @memcpy(bytes_3[0..serialized_bytes_1_count], serialized_message);
+    @memcpy(bytes_3[serialized_bytes_1_count .. serialized_bytes_1_count * 2], serialized_message);
+    @memcpy(bytes_3[serialized_bytes_1_count * 2 .. serialized_bytes_1_count * 3], serialized_message);
 
     const recv_buffer = try parser_messages_allocator.alloc(u8, constants.connection_recv_buffer_size);
     defer parser_messages_allocator.free(recv_buffer);
@@ -69,7 +88,7 @@ test "Parser benchmarks" {
     // would have to deal with for each connection
     var recv_buffer_index: usize = 0;
     while (recv_buffer[recv_buffer_index..].len >= bytes_1.len) {
-        @memcpy(recv_buffer[recv_buffer_index .. recv_buffer_index + bytes_1.len], &bytes_1);
+        @memcpy(recv_buffer[recv_buffer_index .. recv_buffer_index + bytes_1.len], bytes_1);
         recv_buffer_index += bytes_1.len;
     }
 
@@ -95,13 +114,15 @@ test "Parser benchmarks" {
     defer parser_messages_allocator.free(parser_parse_3_title);
 
     // create a single encoded message with no body
-    var message_2 = Message.new();
-    message_2.headers.message_type = .ping;
-    message_2.setTransactionId(1);
+    var message_2 = Message.new(.undefined);
+    const message_2_buf = try parser_messages_allocator.alloc(u8, message_2.packedSize());
+    defer parser_messages_allocator.free(message_2_buf);
 
-    const bytes_4 = try parser_messages_allocator.alloc(u8, message_2.size());
+    const serialized_bytes_2_count = message_2.serialize(message_2_buf);
+
+    const bytes_4 = try parser_messages_allocator.alloc(u8, message_2.packedSize());
     defer parser_messages_allocator.free(bytes_4);
-    message_2.encode(bytes_4);
+    @memcpy(bytes_4[0..serialized_bytes_2_count], message_2_buf[0..serialized_bytes_2_count]);
 
     const parser_parse_4_title = try std.fmt.allocPrint(
         parser_messages_allocator,
@@ -117,9 +138,11 @@ test "Parser benchmarks" {
     );
     defer parser_messages_allocator.free(parser_parse_5_title);
 
+    var messages: [10]Message = undefined;
+
     try bench.addParam(
         parser_parse_4_title,
-        &ParserParseBenchmark.new(&parser_messages, &parser, bytes_4),
+        &ParserParseBenchmark.new(&messages, &parser, bytes_4),
         .{
             .hooks = .{
                 .after_each = afterEach,
@@ -128,7 +151,7 @@ test "Parser benchmarks" {
     );
     try bench.addParam(
         parser_parse_1_title,
-        &ParserParseBenchmark.new(&parser_messages, &parser, &bytes_1),
+        &ParserParseBenchmark.new(&messages, &parser, bytes_1),
         .{
             .hooks = .{
                 .after_each = afterEach,
@@ -137,7 +160,7 @@ test "Parser benchmarks" {
     );
     try bench.addParam(
         parser_parse_2_title,
-        &ParserParseBenchmark.new(&parser_messages, &parser, &bytes_2),
+        &ParserParseBenchmark.new(&messages, &parser, bytes_2),
         .{
             .hooks = .{
                 .after_each = afterEach,
@@ -146,7 +169,7 @@ test "Parser benchmarks" {
     );
     try bench.addParam(
         parser_parse_3_title,
-        &ParserParseBenchmark.new(&parser_messages, &parser, &bytes_3),
+        &ParserParseBenchmark.new(&messages, &parser, bytes_3),
         .{
             .hooks = .{
                 .after_each = afterEach,
@@ -155,7 +178,7 @@ test "Parser benchmarks" {
     );
     try bench.addParam(
         parser_parse_5_title,
-        &ParserParseBenchmark.new(&parser_messages, &parser, recv_buffer[0..recv_buffer_index]),
+        &ParserParseBenchmark.new(&messages, &parser, recv_buffer[0..recv_buffer_index]),
         .{
             .hooks = .{
                 .after_each = afterEach,
@@ -167,8 +190,8 @@ test "Parser benchmarks" {
     const writer = &stderr.interface;
 
     try writer.writeAll("\n");
-    try writer.writeAll("|-------------------|\n");
-    try writer.writeAll("| Parser Benchmarks |\n");
-    try writer.writeAll("|-------------------|\n");
+    try writer.writeAll("|--------------------|\n");
+    try writer.writeAll("| Parser2 Benchmarks |\n");
+    try writer.writeAll("|--------------------|\n");
     try bench.run(writer);
 }
