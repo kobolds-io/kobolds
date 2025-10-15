061cd35b7cfbfbfd2cf9d730171fe1d3e4d92518
Ben Hodge <benjamin.andrew.hodge@gmail.com>
2025-10-15 10:46:28 -0500

    slow tick rate

--- src/constants.zig
+++ src/constants.zig
@@ -35,8 +35,8 @@ pub const connection_send_buffer_size = (1024 * 256);
 
 /// The number of microseconds the IO instance will wait until it flushes
 /// submissions and completions.
-pub const io_tick_us: u63 = 1;
-// pub const io_tick_us: u63 = 100;
+// pub const io_tick_us: u63 = 1;
+pub const io_tick_us: u63 = 100;
 // pub const io_tick_us: u63 = 250;
 // pub const io_tick_us: u63 = 100_000; // testing only
 
@@ -46,8 +46,8 @@ pub const io_uring_entries: u16 = 256;
 /// Optionally verify if operations based on this boolean.
 pub const verify: bool = true;
 
-pub const default_node_memory_pool_capacity: usize = 1_000;
-pub const default_client_memory_pool_capacity: usize = 1_000;
+pub const default_node_memory_pool_capacity: usize = 10_000;
+pub const default_client_memory_pool_capacity: usize = 5_000;
 pub const default_client_outbox_capacity: usize = default_client_memory_pool_capacity;
 pub const default_client_inbox_capacity: usize = default_client_memory_pool_capacity;
 
