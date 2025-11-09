6b787197fba9c9cf8ead8a4b50fcd8616fe6979a
Ben Hodge <benjamin.andrew.hodge@gmail.com>
2025-11-09 11:14:52 -0600

    add initial files

---
+++ build.zig
@@ -0,0 +1,93 @@
+const std = @import("std");
+
+const version = std.SemanticVersion{ .major = 0, .minor = 0, .patch = 0 };
+
+pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
+    setupExecutable(b, target, optimize);
+    setupTests(b, target, optimize);
+}
+
+fn setupExecutable(
+    b: *std.Build,
+    target: std.Build.ResolvedTarget,
+    optimize: std.builtin.OptimizeMode,
+) void {
+    const kobolds_exe = b.addExecutable(.{
+        .name = "kobolds",
+        .root_module = b.createModule(.{
+            .root_source_file = b.path("src/main.zig"),
+            .target = target,
+            .optimize = optimize,
+        }),
+        .version = version,
+    });
+
+    const stdx_dep = b.dependency("stdx", .{
+        .target = target,
+        .optimize = optimize,
+    });
+    const stdx_mod = stdx_dep.module("stdx");
+
+    const kid_dep = b.dependency("kid", .{
+        .target = target,
+        .optimize = optimize,
+    });
+    const kid_mod = kid_dep.module("kid");
+
+    const clap_dep = b.dependency("clap", .{
+        .target = target,
+        .optimize = optimize,
+    });
+    const clap_mod = clap_dep.module("clap");
+
+    kobolds_exe.root_module.addImport("stdx", stdx_mod);
+    kobolds_exe.root_module.addImport("kid", kid_mod);
+    kobolds_exe.root_module.addImport("clap", clap_mod);
+
+    b.installArtifact(kobolds_exe);
+
+    const run_kobolds_exe = b.addRunArtifact(kobolds_exe);
+    const run_step = b.step("run", "Run kobolds");
+    run_step.dependOn(&run_kobolds_exe.step);
+
+    if (b.args) |args| {
+        run_kobolds_exe.addArgs(args);
+    }
+}
+
+fn setupTests(
+    b: *std.Build,
+    target: std.Build.ResolvedTarget,
+    optimize: std.builtin.OptimizeMode,
+) void {
+    const kobolds_unit_tests = b.addTest(.{
+        .root_module = b.createModule(.{
+            .root_source_file = b.path("src/test.zig"),
+            .target = target,
+            .optimize = optimize,
+        }),
+    });
+
+    const stdx_dep = b.dependency("stdx", .{
+        .target = target,
+        .optimize = optimize,
+    });
+    const stdx_mod = stdx_dep.module("stdx");
+
+    const kid_dep = b.dependency("kid", .{
+        .target = target,
+        .optimize = optimize,
+    });
+    const kid_mod = kid_dep.module("kid");
+
+    kobolds_unit_tests.root_module.addImport("stdx", stdx_mod);
+    kobolds_unit_tests.root_module.addImport("kid", kid_mod);
+
+    const run_unit_tests = b.addRunArtifact(kobolds_unit_tests);
+    const test_step = b.step("test", "Run unit tests");
+
+    test_step.dependOn(&run_unit_tests.step);
+}
