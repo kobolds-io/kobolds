6306f285f7e61617ce1fcb230f77f6f5f2df900e
Ben Hodge <bengarishodge@pm.me>
2025-11-10 11:08:30 -0600

    add missing tests

--- src/test.zig
+++ src/test.zig
@@ -1,5 +1,7 @@
 comptime {
     _ = @import("./lib/root.zig");
     _ = @import("./lib/frame.zig");
+    _ = @import("./lib/checksum.zig");
+    _ = @import("./lib/protocol.zig");
     _ = @import("./main.zig");
 }
