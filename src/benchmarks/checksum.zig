52e60f72c35c547994d950e9051de9603942a711
Ben Hodge <bengarishodge@pm.me>
2025-11-10 10:33:44 -0600

    add benchmarks for checksum

--- src/benchmarks/checksum.zig
+++ src/benchmarks/checksum.zig
@@ -7,80 +7,119 @@ const testing = std.testing;
 
 const check = @import("../lib/checksum.zig");
 
-pub fn BenchmarkXxHash32Checksum16Bytes(_: std.mem.Allocator) void {
+pub fn BenchmarkXxHash32ChecksumTinyBytes(_: std.mem.Allocator) void {
     const test_bytes: [16]u8 = [_]u8{1} ** 16;
 
     _ = check.xxHash32Checksum(&test_bytes);
 }
 
-pub fn BenchmarkXxHash32Checksum128Bytes(_: std.mem.Allocator) void {
-    const test_bytes: [128]u8 = [_]u8{1} ** 128;
+pub fn BenchmarkXxHash32ChecksumMediumBytes(_: std.mem.Allocator) void {
+    const test_bytes: [std.math.maxInt(u8)]u8 = [_]u8{1} ** std.math.maxInt(u8);
 
     _ = check.xxHash32Checksum(&test_bytes);
 }
 
-pub fn BenchmarkXxHash32Verify16Bytes(_: std.mem.Allocator) void {
+pub fn BenchmarkXxHash32ChecksumLargeBytes(_: std.mem.Allocator) void {
+    const test_bytes: [std.math.maxInt(u16)]u8 = [_]u8{1} ** std.math.maxInt(u16);
+
+    _ = check.xxHash32Checksum(&test_bytes);
+}
+
+pub fn BenchmarkXxHash32VerifyTinyBytes(_: std.mem.Allocator) void {
     const test_bytes: [16]u8 = [_]u8{1} ** 16;
     const sum: u32 = 2876315459;
 
     assert(check.xxHash32Verify(sum, &test_bytes));
 }
 
-pub fn BenchmarkXxHash32Verify128Bytes(_: std.mem.Allocator) void {
-    const test_bytes: [128]u8 = [_]u8{1} ** 128;
-    const sum: u32 = 1092953798;
+pub fn BenchmarkXxHash32VerifyMediumBytes(_: std.mem.Allocator) void {
+    const test_bytes: [std.math.maxInt(u8)]u8 = [_]u8{1} ** std.math.maxInt(u8);
+    const sum: u32 = 2186680621;
 
     assert(check.xxHash32Verify(sum, &test_bytes));
 }
 
-pub fn BenchmarkXxHash64Checksum16Bytes(_: std.mem.Allocator) void {
+pub fn BenchmarkXxHash32VerifyLargeBytes(_: std.mem.Allocator) void {
+    const test_bytes: [std.math.maxInt(u16)]u8 = [_]u8{1} ** std.math.maxInt(u16);
+    const sum: u32 = 871297165;
+
+    assert(check.xxHash32Verify(sum, &test_bytes));
+}
+
+pub fn BenchmarkXxHash64ChecksumTinyBytes(_: std.mem.Allocator) void {
     const test_bytes: [16]u8 = [_]u8{1} ** 16;
 
     _ = check.xxHash64Checksum(&test_bytes);
 }
 
-pub fn BenchmarkXxHash64Checksum128Bytes(_: std.mem.Allocator) void {
-    const test_bytes: [128]u8 = [_]u8{1} ** 128;
+pub fn BenchmarkXxHash64ChecksumMediumBytes(_: std.mem.Allocator) void {
+    const test_bytes: [std.math.maxInt(u8)]u8 = [_]u8{1} ** std.math.maxInt(u8);
 
     _ = check.xxHash64Checksum(&test_bytes);
 }
 
-pub fn BenchmarkXxHash64Verify16Bytes(_: std.mem.Allocator) void {
+pub fn BenchmarkXxHash64ChecksumLargeBytes(_: std.mem.Allocator) void {
+    const test_bytes: [std.math.maxInt(u16)]u8 = [_]u8{1} ** std.math.maxInt(u16);
+
+    _ = check.xxHash64Checksum(&test_bytes);
+}
+
+pub fn BenchmarkXxHash64VerifyTinyBytes(_: std.mem.Allocator) void {
     const test_bytes: [16]u8 = [_]u8{1} ** 16;
     const sum: u64 = 10140113291757988233;
 
     assert(check.xxHash64Verify(sum, &test_bytes));
 }
 
-pub fn BenchmarkXxHash64Verify128Bytes(_: std.mem.Allocator) void {
-    const test_bytes: [128]u8 = [_]u8{1} ** 128;
-    const sum: u64 = 14436252957693351467;
+pub fn BenchmarkXxHash64VerifyMediumBytes(_: std.mem.Allocator) void {
+    const test_bytes: [std.math.maxInt(u8)]u8 = [_]u8{1} ** std.math.maxInt(u8);
+    const sum: u64 = 3581703012154252652;
 
     assert(check.xxHash64Verify(sum, &test_bytes));
 }
 
-pub fn BenchmarkCRC32Checksum16Bytes(_: std.mem.Allocator) void {
+pub fn BenchmarkXxHash64VerifyLargeBytes(_: std.mem.Allocator) void {
+    const test_bytes: [std.math.maxInt(u16)]u8 = [_]u8{1} ** std.math.maxInt(u16);
+    const sum: u64 = 7904164552034802651;
+
+    assert(check.xxHash64Verify(sum, &test_bytes));
+}
+
+pub fn BenchmarkCRC32ChecksumTinyBytes(_: std.mem.Allocator) void {
     const test_bytes: [16]u8 = [_]u8{1} ** 16;
 
     _ = check.checksumCrc32(&test_bytes);
 }
 
-pub fn BenchmarkCRC32Checksum128Bytes(_: std.mem.Allocator) void {
-    const test_bytes: [128]u8 = [_]u8{1} ** 128;
+pub fn BenchmarkCRC32ChecksumMediumBytes(_: std.mem.Allocator) void {
+    const test_bytes: [std.math.maxInt(u8)]u8 = [_]u8{1} ** std.math.maxInt(u8);
 
     _ = check.checksumCrc32(&test_bytes);
 }
 
-pub fn BenchmarkCRC32Verify16Bytes(_: std.mem.Allocator) void {
+pub fn BenchmarkCRC32ChecksumLargeBytes(_: std.mem.Allocator) void {
+    const test_bytes: [std.math.maxInt(u16)]u8 = [_]u8{1} ** std.math.maxInt(u16);
+
+    _ = check.checksumCrc32(&test_bytes);
+}
+
+pub fn BenchmarkCRC32VerifyTinyBytes(_: std.mem.Allocator) void {
     const test_bytes: [16]u8 = [_]u8{1} ** 16;
     const sum: u32 = 1386227895;
 
     assert(check.verifyCrc32(sum, &test_bytes));
 }
 
-pub fn BenchmarkCRC32Verify128Bytes(_: std.mem.Allocator) void {
-    const test_bytes: [128]u8 = [_]u8{1} ** 128;
-    const sum: u32 = 890172475;
+pub fn BenchmarkCRC32VerifyMediumBytes(_: std.mem.Allocator) void {
+    const test_bytes: [std.math.maxInt(u8)]u8 = [_]u8{1} ** std.math.maxInt(u8);
+    const sum: u32 = 1444046329;
+
+    assert(check.verifyCrc32(sum, &test_bytes));
+}
+
+pub fn BenchmarkCRC32VerifyLargeBytes(_: std.mem.Allocator) void {
+    const test_bytes: [std.math.maxInt(u16)]u8 = [_]u8{1} ** std.math.maxInt(u16);
+    const sum: u32 = 1040761532;
 
     assert(check.verifyCrc32(sum, &test_bytes));
 }
@@ -91,12 +130,15 @@ test "checksum benchmarks" {
     });
     defer bench.deinit();
 
-    try bench.add("xxhash32 checksum 16 bytes", BenchmarkXxHash32Checksum16Bytes, .{});
-    try bench.add("xxhash32 checksum 128 bytes", BenchmarkXxHash32Checksum128Bytes, .{});
-    try bench.add("xxhash64 checksum 16 bytes", BenchmarkXxHash64Checksum16Bytes, .{});
-    try bench.add("xxhash64 checksum 128 bytes", BenchmarkXxHash64Checksum128Bytes, .{});
-    try bench.add("crc32 checksum 16 bytes", BenchmarkCRC32Checksum16Bytes, .{});
-    try bench.add("crc32 checksum 128 bytes", BenchmarkCRC32Checksum128Bytes, .{});
+    try bench.add("xxhash32 checksum tiny bytes", BenchmarkXxHash32ChecksumTinyBytes, .{});
+    try bench.add("xxhash32 checksum medium bytes", BenchmarkXxHash32ChecksumMediumBytes, .{});
+    try bench.add("xxhash32 checksum large bytes", BenchmarkXxHash32ChecksumLargeBytes, .{});
+    try bench.add("xxhash64 checksum tiny bytes", BenchmarkXxHash64ChecksumTinyBytes, .{});
+    try bench.add("xxhash64 checksum medium bytes", BenchmarkXxHash64ChecksumMediumBytes, .{});
+    try bench.add("xxhash64 checksum large bytes", BenchmarkXxHash64ChecksumLargeBytes, .{});
+    try bench.add("crc32 checksum tiny bytes", BenchmarkCRC32ChecksumTinyBytes, .{});
+    try bench.add("crc32 checksum medium bytes", BenchmarkCRC32ChecksumMediumBytes, .{});
+    try bench.add("crc32 checksum large bytes", BenchmarkCRC32ChecksumLargeBytes, .{});
 
     var stderr = std.fs.File.stderr().writerStreaming(&.{});
     const writer = &stderr.interface;
@@ -114,12 +156,15 @@ test "verify benchmarks" {
     });
     defer bench.deinit();
 
-    try bench.add("xxhash32 verify 16 bytes", BenchmarkXxHash32Verify16Bytes, .{});
-    try bench.add("xxhash32 verify 128 bytes", BenchmarkXxHash32Verify128Bytes, .{});
-    try bench.add("xxhash64 verify 16 bytes", BenchmarkXxHash64Verify16Bytes, .{});
-    try bench.add("xxhash64 verify 128 bytes", BenchmarkXxHash64Verify128Bytes, .{});
-    try bench.add("crc32 verify 16 bytes", BenchmarkCRC32Verify16Bytes, .{});
-    try bench.add("crc32 verify 128 bytes", BenchmarkCRC32Verify128Bytes, .{});
+    try bench.add("xxhash32 verify tiny bytes", BenchmarkXxHash32VerifyTinyBytes, .{});
+    try bench.add("xxhash32 verify medium bytes", BenchmarkXxHash32VerifyMediumBytes, .{});
+    try bench.add("xxhash32 verify large bytes", BenchmarkXxHash32VerifyLargeBytes, .{});
+    try bench.add("xxhash64 verify tiny bytes", BenchmarkXxHash64VerifyTinyBytes, .{});
+    try bench.add("xxhash64 verify medium bytes", BenchmarkXxHash64VerifyMediumBytes, .{});
+    try bench.add("xxhash64 verify large bytes", BenchmarkXxHash64VerifyLargeBytes, .{});
+    try bench.add("crc32 verify tiny bytes", BenchmarkCRC32VerifyTinyBytes, .{});
+    try bench.add("crc32 verify medium bytes", BenchmarkCRC32VerifyMediumBytes, .{});
+    try bench.add("crc32 verify large bytes", BenchmarkCRC32VerifyLargeBytes, .{});
 
     var stderr = std.fs.File.stderr().writerStreaming(&.{});
     const writer = &stderr.interface;
