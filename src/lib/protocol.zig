924835af09ef17909f37cc27c8d77f5b5f0511a7
Ben Hodge <bengarishodge@pm.me>
2025-11-10 11:08:14 -0600

    add a test for ProtocolVersion checking

--- src/lib/protocol.zig
+++ src/lib/protocol.zig
@@ -1,3 +1,6 @@
+const std = @import("std");
+const testing = std.testing;
+
 pub const ProtocolVersion = enum(u4) {
     unsupported,
     v1,
@@ -9,3 +12,14 @@ pub const ProtocolVersion = enum(u4) {
         };
     }
 };
+
+test "protocol version fromBits" {
+    const unsupported_bits: u4 = 0;
+    try testing.expectEqual(.unsupported, ProtocolVersion.fromBits(unsupported_bits));
+
+    const v1_bits: u4 = 1;
+    try testing.expectEqual(.v1, ProtocolVersion.fromBits(v1_bits));
+
+    const unknown_bits: u4 = 15;
+    try testing.expectEqual(.unsupported, ProtocolVersion.fromBits(unknown_bits));
+}
