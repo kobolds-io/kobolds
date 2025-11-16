e02b14fb4aa5fc31cc92f95cd142233cc43ac1c9
Ben Hodge <benjamin.andrew.hodge@gmail.com>
2025-11-15 23:20:01 -0600

    add tests

--- src/test.zig
+++ src/test.zig
@@ -1,8 +1,10 @@
 comptime {
-    _ = @import("./lib/root.zig");
-    _ = @import("./lib/frame.zig");
     _ = @import("./lib/checksum.zig");
-    _ = @import("./lib/protocol.zig");
+    _ = @import("./lib/chunk.zig");
+    _ = @import("./lib/frame.zig");
+    _ = @import("./lib/message_parser.zig");
     _ = @import("./lib/message.zig");
+    _ = @import("./lib/protocol.zig");
+    _ = @import("./lib/root.zig");
     _ = @import("./main.zig");
 }
